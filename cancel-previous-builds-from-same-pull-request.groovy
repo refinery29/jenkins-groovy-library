2bcbbbc1be13ea381e3ba541675b168c8eac0066
Schlueter <bs@schlueter.blue>
2017-12-06 18:03:09 -0500

    Display build status url of aborted build

--- cancel-previous-builds-from-same-pull-request.groovy
+++ cancel-previous-builds-from-same-pull-request.groovy
@@ -29,7 +29,7 @@ build.getProject()._getRuns().each { _, run ->
             cause = new CauseOfInterruption.UserInterruption("Aborted by #${build.number}")
             exec.interrupt(Result.ABORTED, cause)
 
-            println "Aborted duplicate ${run.getClass()}"
+            println "Aborted duplicate ${run.getBuildStatusUrl()}"
         }
     }
 }
