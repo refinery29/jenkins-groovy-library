37345f10d813ee86d7adc06c524404c0671365e9
Schlueter <bs@schlueter.blue>
2017-12-06 18:11:21 -0500

    Do reference better

--- cancel-previous-builds-from-same-pull-request.groovy
+++ cancel-previous-builds-from-same-pull-request.groovy
@@ -30,7 +30,7 @@ build.getProject()._getRuns().each { _, run ->
             cause = new CauseOfInterruption.UserInterruption("Aborted by #${build.number}")
             exec.interrupt(Result.ABORTED, cause)
 
-            println "Aborted duplicate ${run} (${Hudson.instance.getRootUrl() + run.getUrl()})"
+            println "Aborted duplicate #${run.number}"
         }
     }
 }
