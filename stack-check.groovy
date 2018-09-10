ddaded822d3ecfdd79882a9b192e93864a30f736
Schlueter <b@schlueter.blue>
2018-09-10 13:33:04 -0400

    Prepend interrupt with stack_

--- stack-check.groovy
+++ stack-check.groovy
@@ -3,7 +3,7 @@ env = build.getEnvironment()
 stack = env.get('stack')
 stack_name = env.get('stack_name')
 
-def interrupt(message) {
+def stack_interrupt(message) {
     msg = 'ERROR: ' + message
     build.displayName = msg
     println(msg)
@@ -12,10 +12,10 @@ def interrupt(message) {
 
 // Abort the build if no stack set.
 if (stack == '' && stack_name == '') {
-    interrupt('No stack specified')
+    stack_interrupt('No stack specified')
 }
 
 // Abort if both stack and stack name are provided.
 if (stack != '' && stack_name != '') {
-    interrupt('Both stack and stack_name provided')
+    stack_interrupt('Both stack and stack_name provided')
 }
